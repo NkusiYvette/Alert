d4fc416da1504b04013067f361b442c727d15ad1
UWIMPAYE Yvette <nkusiyvette1@gmail.com>
2023-12-11 12:40:00 +0200

    new

--- fun.js
+++ fun.js
@@ -1,15 +1,22 @@
-let temperatures;
+let temperatures = [];
 let sum;
+let numT;
 let meanTemp;
+
+function getTemp(){
+numT = Number(prompt("How many total temperatures do you want to enter: ", numT));
+for (let i = 0; i < numT; i++){
+    temperatures[i] = Number(prompt(`Enter Temperature ${i} :`));
+}}
+
 function getMeanTemp(){
     sum = 0;
-    for(let i =0; i < temperatures.length; i++){
+    for(let i = 0; i < temperatures.length; i++){
         sum +=temperatures[i];
     }
     meanTemp = sum / temperatures.length;
 }
 
-temperatures = [20,5,12,30,56];
-
+getTemp();
 getMeanTemp();
 console.log(`Mean Temp : ${meanTemp}`);
